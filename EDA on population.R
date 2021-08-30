5cf7dae1d9c4c4a98323a830e285558867df0517
navidm108 <83680301+navidm108@users.noreply.github.com>
2021-08-30 17:59:05 +1000

    EDA on ABS_ERP_LGA_CSV file.(Population)

---
+++ EDA on population.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(readxl)
+library(xlsx)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(zoo)
+library(corrplot)
+library(ggplot2)
+library(scales)
+
+
+#Get working directory
+getwd()
+
+#Read CSV file
+LGA<-read.csv("ABS_ERP_LGA2019_27082021235324501.csv")
+
+
+#Check columns' name
+colnames(LGA)
+
+#Drop unuseable Variables
+LGA<- LGA[, !(colnames(LGA) %in% c("ï..MEASURE","Geography.Level","FREQUENCY","Frequency" ,"TIME","Flag.Codes","Flags"))]
+
+
+#Get data summary
+str(LGA)
+summary(LGA)
+
+#Check unique Region
+unique(LGA$Region)
+unique(LGA$LGA_2019)
+
+
+
+#Visualize Region column
+
+g <- ggplot(LGA[-c(1:19),], aes(Time, Value))
+g + geom_bar(stat="identity", width = 0.5, fill="tomato2") +
+  scale_y_continuous(labels = comma) +
+  scale_x_continuous(breaks = seq(from = 2000, to = 2020, by = 1))+
+  labs(title="Bar Chart", 
+       subtitle="NSW population per Year" ) +
+  theme(axis.text.x = element_text(angle=65, vjust=0.6))
+
+
+for (i in unique(LGA$Region)){
+  print(ggplot(LGA[LGA$Region==i,], aes(Time, Value))+
+    geom_bar(stat="identity", width = 0.5, fill="tomato2") +
+      facet_wrap( ~ Region)+
+    scale_y_continuous(labels = comma) +
+    scale_x_continuous(breaks = seq(from = 2000, to = 2020, by = 1))+
+    labs(title="population per year at", 
+         subtitle=i ) +
+    theme(axis.text.x = element_text(angle=65, vjust=0.6)))
+}
+
+
+
+
