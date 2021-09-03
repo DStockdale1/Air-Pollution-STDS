4dd001f81616f75e795422332ecfce7bd99792cf
williamMcdUTS <william.t.mcdermott@student.uts.edu.au>
2021-09-03 18:15:13 +1000

    Population Density Map

--- Datasets to merge/Data Load Script.R
+++ Datasets to merge/Data Load Script.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(readxl)
+library(geosphere)
+library(leaflet)
 
 # This script loads the data sets we will be using (except the zoning until we find that)
 
@@ -21,8 +23,8 @@ Traffic_Data <- read_csv("Raw Data/Traffic_Volume_Viewer_2007-2021.csv",
 
 
 {
-  load("Datasets to merge/Raw Data/AQI_Data.Rda")
-  load("Datasets to merge/Raw Data/AQI Data/Air Quality Site Details.Rda")
+  load("Raw Data/AQI_Data.Rda")
+  load("Raw Data/AQI Data/Air Quality Site Details.Rda")
 } # Load AQI Data
 
 AQI_Summarized_Data <- AQI_Summarized_Data %>% 
@@ -44,3 +46,26 @@ Randwick <- df_site_details %>%
   + select(SiteName, Longitude, Latitude)
 
 distHaversine(c(Randwick$Longitude, Randwick$Latitude), c(Rozelle$Longitude, Rozelle$Latitude)) # Result is in metres
+
+distHaversine(c(AQI_Summarized_Data$Longitude, AQI_Summarized_Data$Latitude), c(Traffic_Data$wgs84_longitude, Traffic_Data$wgs84_latitude))
+
+AQI_Sites <- AQI_Summarized_Data %>% 
+  transmute(AQI_Site_Id = Site_Id, AQI_Long = Longitude, AQI_Lat = Latitude) %>% 
+  distinct(AQI_Site_Id, AQI_Long, AQI_Lat)
+
+Traffic_Sites <- Traffic_Data %>%
+  transmute(Traffic_Site_Id = station_id, Traff_Long = wgs84_longitude, Traff_Lat = wgs84_latitude) %>% 
+  distinct(Traffic_Site_Id, Traff_Long, Traff_Lat)
+
+my_distm <- function(long1, lat1, long2, lat2)
+  distm(c(long1, lat1), c(long2, lat2), fun=distHaversine)
+
+Crossjoined_AQI_Traffic <- merge(AQI_Sites, Traffic_Sites) %>% 
+  arrange(AQI_Site_Id) %>% 
+  rowwise() %>% 
+  mutate(distance = my_distm(AQI_Long, AQI_Lat, Traff_Long, Traff_Lat)/1000)
+
+boxplot(Crossjoined_AQI_Traffic$distance)
+
+
+
